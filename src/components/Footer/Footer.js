0f904ad85e7d981dfc9d3ce54a478c78a42745ad
Prathibha R <prathi2kgrs@gmail.com>
2021-07-15 22:12:52 +0530

    adding all files and folders

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://www.linkedin.com/in/prathibhar1441"
+          style={{ cursor: "pointer" }}
+          target="_blank"
+        >
+          Prathibha R
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
